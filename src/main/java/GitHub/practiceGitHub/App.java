f67ec320afd9f580575272e6afe73930e75c71e2
HaroonDawlatzai <haroondawlatzai@gmail.com>
2021-07-26 21:22:47 -0400

    this is my second commit

---
+++ src/main/java/GitHub/practiceGitHub/App.java
@@ -0,0 +1,13 @@
+package GitHub.practiceGitHub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
